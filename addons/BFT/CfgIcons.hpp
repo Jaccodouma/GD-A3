96af4c6a9c71522e2375693a388bb13b97fa5d11
Jacco Douma <jaccodouma@gmail.com>
2021-08-14 00:30:12 +0200

    Readded icons that are still being used (oops)

---
+++ addons/BFT/CfgIcons.hpp
@@ -0,0 +1,8 @@
+class TCA_BFT_Interact_Icons {
+	BFT = QPATHTO_T(UI\icons\BFT.paa);
+	colorWheel = QPATHTO_T(UI\icons\colorWheel.paa);
+	on = QPATHTO_T(UI\icons\on.paa);
+	off = QPATHTO_T(UI\icons\off.paa);
+	pen = QPATHTO_T(UI\icons\pen.paa);
+	plus = QPATHTO_T(UI\icons\plus.paa);
+};
