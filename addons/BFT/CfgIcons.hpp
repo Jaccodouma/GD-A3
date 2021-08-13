ec959ef18130ee0dbba95eca0a600668b2ad778d
Jacco Douma <jaccodouma@gmail.com>
2021-08-14 01:29:15 +0200

    Added current colour to name selection

--- addons/BFT/CfgIcons.hpp
+++ addons/BFT/CfgIcons.hpp
@@ -1,3 +1,5 @@
+#define LETTER_ENTRY(LTTR) LTTR = QPATHTO_T(UI\icons\letter\LTTR.paa)
+
 class TCA_BFT_Interact_Icons {
 	BFT = QPATHTO_T(UI\icons\BFT.paa);
 	colorWheel = QPATHTO_T(UI\icons\colorWheel.paa);
@@ -5,4 +7,32 @@ class TCA_BFT_Interact_Icons {
 	off = QPATHTO_T(UI\icons\off.paa);
 	pen = QPATHTO_T(UI\icons\pen.paa);
 	plus = QPATHTO_T(UI\icons\plus.paa);
+	class letter {
+		LETTER_ENTRY(A);
+		LETTER_ENTRY(B);
+		LETTER_ENTRY(C);
+		LETTER_ENTRY(D);
+		LETTER_ENTRY(E);
+		LETTER_ENTRY(F);
+		LETTER_ENTRY(G);
+		LETTER_ENTRY(H);
+		LETTER_ENTRY(I);
+		LETTER_ENTRY(J);
+		LETTER_ENTRY(K);
+		LETTER_ENTRY(L);
+		LETTER_ENTRY(M);
+		LETTER_ENTRY(N);
+		LETTER_ENTRY(O);
+		LETTER_ENTRY(P);
+		LETTER_ENTRY(Q);
+		LETTER_ENTRY(R);
+		LETTER_ENTRY(S);
+		LETTER_ENTRY(T);
+		LETTER_ENTRY(U);
+		LETTER_ENTRY(V);
+		LETTER_ENTRY(W);
+		LETTER_ENTRY(X);
+		LETTER_ENTRY(Y);
+		LETTER_ENTRY(Z);
+	}
 };
--- addons/BFT/functions/fnc_groupMarkers_addSettings.sqf
+++ addons/BFT/functions/fnc_groupMarkers_addSettings.sqf
@@ -67,10 +67,17 @@ _action_BFT_Name_insertChildren = {
 			(group player) setGroupIdGlobal [_params];
 		};
 
+		_icon = getText(configfile >> "TCA_BFT_Interact_Icons" >> "letter" >> toUpper _x select [0,1]);
+    	
+		// Get icon colour in CfgMarkerColors, convert to RGB array, then convert to HTML string
+		_iconColour = group player getVariable ["BFT_groupMarker_color", [playerSide, true] call BIS_fnc_sideColor];
+		_iconColour = (getArray (configfile >> "CfgMarkerColors" >> _iconColour >> "color")) call BIS_fnc_colorConfigToRGBA;
+		_iconColour = _iconColour call BIS_fnc_colorRGBtoHTML;
+
 		_action = [
 			format ["Jacco_BFT_Names_%1", _x],
 			_x, 
-			"", 
+			[_icon, _iconColour], 
 			_statement, 
 			{true}, 
 			{}, 
