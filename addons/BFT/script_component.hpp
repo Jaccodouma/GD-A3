d4a443d0e71654907c8de4c84881b4b3314c368c
Jacco Douma <jaccodouma@gmail.com>
2021-08-14 14:46:00 +0200

    Update script_component.hpp

--- addons/BFT/script_component.hpp
+++ addons/BFT/script_component.hpp
@@ -4,10 +4,10 @@
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 
-#ifdef DEBUG_ENABLED_MAIN
+#ifdef DEBUG_ENABLED_BFT
     #define DEBUG_MODE_FULL
 #endif
-    #ifdef DEBUG_SETTINGS_MAIN
+    #ifdef DEBUG_SETTINGS_BFT
     #define DEBUG_SETTINGS DEBUG_SETTINGS_MAIN
 #endif
 
