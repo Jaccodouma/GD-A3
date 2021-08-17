98d5400dac68f43c6d018ef25aed61615c7a79a9
Jacco Douma <jaccodouma@gmail.com>
2021-08-17 17:36:41 +0200

    Separated unit markers into own pbo

--- addons/BFT/XEH_PREP.hpp
+++ addons/BFT/XEH_PREP.hpp
@@ -5,8 +5,6 @@ PREP(groupMarkers_remove);
 PREP(groupMarkers_addSettings);
 PREP(groupMarkers_getGroupPosition);
 
-PREP(unitMarkers_init);
-
 PREP(groupMarkers_settings_insertIconsChildren);
 PREP(groupMarkers_settings_insertColorsChildren);
 PREP(groupMarkers_settings_insertNameChildren);
